de05af8b16a7cddfdf31d6e7f2ef4920d468424a
Sean <seanpkelley94@gmail.com>
2023-03-15 14:02:59 -0400

    updated heuristics

--- src/cut/VwsSolverInterface.cpp
+++ src/cut/VwsSolverInterface.cpp
@@ -85,7 +85,8 @@ std::shared_ptr<CbcModel> VwsSolverInterface::unprocessedBranchAndCut(
   CbcMain0(*model, cbcData);
   cbcData[CbcParam::DIVEOPT]->setVal(2);
   cbcData[CbcParam::FPUMPITS]->setVal(30);
-  cbcData[CbcParam::FPUMPTUNE]->setVal(1005043);
+  // turn off FPUMP heuristic because it takes too long
+//  cbcData[CbcParam::FPUMPTUNE]->setVal(1005043);
   cbcData[CbcParam::DIVINGC]->setVal("on");
   cbcData[CbcParam::RINS]->setVal("on");
   doHeuristics(model.get(), 1, cbcData, cbcData.noPrinting(), 1005043);
