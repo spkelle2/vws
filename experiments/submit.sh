b3972baa17d5b9a2be3e3fab1c8cec4dcf1c2f7f
Sean Kelley <seanpkelley94@gmail.com>
2023-12-13 16:11:37 -0600

    updating submit.sh

--- experiments/submit.sh
+++ experiments/submit.sh
@@ -3,7 +3,6 @@
 cd /home/sek519/warm_starting/vws/experiments
 source /home/sek519/.bashrc
 source /home/sek519/miniconda/bin/activate
-conda activate vws
 
 # Your command
 ../Debug/vws ${INPUT_FOLDER} ${OUTPUT_FILE} ${MAX_TIME} ${GENERATOR} ${TERMS} ${MIP_SOLVER} ${PROVIDE_PRIMAL_BOUND}
