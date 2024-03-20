6e55bc88654c87653df8432f76a04e4902b45819
Sean Kelley <seanpkelley94@gmail.com>
2024-03-20 16:41:27 -0400

    updated result reader made new set

--- experiments/copy_test_sets.py
+++ experiments/copy_test_sets.py
@@ -1,42 +1,26 @@
 import os
 import shutil
 
-instance = False
+# copy the test sets
+# set parameters
+source = "test_sets/miplib_2017_5000"
+dest = "test_sets/miplib_2017_subset"
+source_instances = "instances/miplib_2017_5000"
+dest_instances = "instances/miplib_2017_subset"
+file = "miplib_2017_subset_names.txt"
 
-if not instance:
+count = 0
 
-    # set parameters
-    source = "test_sets/miplib_5000"
-    dest = "test_sets/miplib_2017_5000"
-    file = "miplib_2017_names.txt"
+# read file into a string
+with open(file, "r") as f:
+    test_names = f.read().lower()
 
-    count = 0
+for fldr in os.listdir(source):
+    if os.path.isdir(os.path.join(source, fldr)) and fldr in test_names:
+        print("copying", fldr)
+        shutil.copytree(os.path.join(source, fldr), os.path.join(dest, fldr))
+        shutil.copy(os.path.join(source_instances, f"{fldr}.mps"),
+                    os.path.join(dest_instances, f"{fldr}.mps"))
+        count += 1
 
-    # read file into a string
-    with open(file, "r") as f:
-        test_names = f.read().lower()
-
-    for fldr in os.listdir(source):
-        if os.path.isdir(os.path.join(source, fldr)) and fldr in test_names:
-            print("copying", fldr)
-            shutil.copytree(os.path.join(source, fldr), os.path.join(dest, fldr))
-            count += 1
-
-    print(count, " folders copied")
-
-else:
-    # set parameters
-    source = "instances/miplib_5000"
-    dest = "instances/miplib_2017_5000"
-    fldr = "../../miplib2017"
-    fldr_files = os.listdir(fldr)
-
-    count = 0
-
-    for instance in os.listdir(source):
-        if instance in fldr_files:
-            print("copying", instance)
-            shutil.copy(os.path.join(source, instance), os.path.join(dest, instance))
-            count += 1
-
-    print(count, " instances copied")
+print(count, " folders copied")
